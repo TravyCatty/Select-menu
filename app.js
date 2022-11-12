a835d6f44ff2e581645c53ce8f6c0d5f7c3d8e6c
Leonid <leonid.bgh@gmail.com>
2022-11-12 19:21:34 +0500

    add up arrow

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const select = document.querySelector('.select')
 const options_list = document.querySelector('.options-list')
 const option = document.querySelector('.option')
 
+
 // Show & hide options list
 
 select.addEventListener('click', () => {
     options_list.classList.toggle('active')
+    select.querySelector('.fa-angle-down').classList.toggle('fa-angle-up')
 })
+
+// select option
+
